c9b296c936aa03435e51f7eec65b666304328533
Edward Ezekiel <ed.a.ezekiel@gmail.com>
2018-11-28 08:05:40 -0500

    Done.

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe Post, type: :model do
-  let(:content) { "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed dapibus, nulla vel condimentum ornare, arcu lorem hendrerit purus, ac sagittis ipsum nisl nec erat. Morbi porta sollicitudin leo, eu cursus libero posuere ac. Sed ac ultricies ante. Donec nec nulla ipsum. Nunc eleifend, ligula ut volutpat." }
+  let(:content) { "Lorem ipsuom loquitor blah blah blah" }
 
   let(:valid_attrs) do
     {
